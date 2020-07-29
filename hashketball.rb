6cd2f9e2d67abd4ec8f129c1b132eb73115d3eb7
Arpita Dutta <duttaarpita22@gmail.com>
2020-07-29 02:15:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,7 +19,17 @@ def game_hash
     :away=>{
      :team_name=>"",
      :colors=>[],
-     :players=>[{},{}]
+     :players=>[{
+       :player_name=>"",
+       :number=>,
+       :shoe=>,
+       :points=>,
+       :rebounds=>,
+       :assists=>,
+       :steals=>,
+       :blocks=>,
+       :slam_dunks=>
+     },{}]
     }
   }
 end
